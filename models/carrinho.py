1314a3eb3914ac35a093a5dd114bf80b1e107378
Vicente Pilonetto <vicente.pilonetto@estudantes.ifc.edu.br>
2025-11-18 20:39:01 -0300

    Adicionando a classe carrinho

---
+++ models/carrinho.py
@@ -0,0 +1,20 @@
+class Carrinho:
+    def __init__(self, quantidade_itens: int, produtos: list, limite: int):
+        self.quantidade_itens = quantidade_itens
+        self.produtos = []
+        self.limite = limite
+
+    def adicionar_produto(self, produto):
+        if self.quantidade_itens < self.limite:
+            self.produtos.append(produto)
+            self.quantidade_itens += 1
+
+    def remover_produto(self, produto):
+        if produto in self.produtos:
+            self.produtos.remove(produto)
+            self.quantidade_itens -= 1
+
+    def limpar_carrinho(self):
+        self.produtos = []
+        self.quantidade_itens = 0
+
