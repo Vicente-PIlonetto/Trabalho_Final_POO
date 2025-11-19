5f79a92ba0afdf4d3f2aef3bfef876bdf3af4948
CarlosSouzaAmorim <carlos.souza.amorim@gmail.com>
2025-11-18 21:44:40 -0300

    Modificado classe pedido (import).

--- models/pedido.py
+++ models/pedido.py
@@ -1,11 +1,14 @@
+from models.pagamento import Pagamento
+from models.carrinho import Carrinho
+
 class Pedido:
     def __init__(self, id_pedido: int, preco:float, carrinho: Carrinho):
         self.id_pedido = id_pedido
         self.preco = preco
         self.carrinho = carrinho
         
-    def realizar_pagamento(self, pagamento: Pagamento) -> bool:
-        return pagamento.realizar_pagamento()
+    def realizar_pagamento(self) -> Pagamento:
+        pass
 
 
 
