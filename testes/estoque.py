fec761b1efd9e842bf74c08bcfaf15183ae3907a
giovanipott <giovanipott@gmail.com>
2025-11-18 22:11:42 -0300

    Adicionando teste estoque

---
+++ testes/estoque.py
@@ -0,0 +1,10 @@
+from models.estoque import Estoque
+from models.produto import Alimento
+
+Estoque1 = Estoque(50)
+Produto1 = Alimento(1, "Banana", 5, 1, 6.99, 1, 1, 1)
+
+Estoque1.armazenar(Produto1)
+
+Estoque1.remove(Produto1)
+
